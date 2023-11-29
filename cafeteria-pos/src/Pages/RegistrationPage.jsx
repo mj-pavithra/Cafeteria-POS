fe2df7af46e4dc7c713595a46725a123c0889cb4
MJ Pavithra <mjpavithra6520@gmail.com>
2023-11-30 01:49:17 +0530

    Registation page completed

--- cafeteria-pos/src/Pages/RegistrationPage.jsx
+++ cafeteria-pos/src/Pages/RegistrationPage.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import RegistrationCard from "../Components/RegistrationCard";
+import "../css/RegistrationPage.css";
 
 function RegistrationPage() {
     return (
-        <div>
-            <RegistrationCard />
+        <div className="register-frame">
+            <RegistrationCard className="test"/>
         </div>
     );
 }
