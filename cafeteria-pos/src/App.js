9524acc14c03878e1eeb6243d98e2986d65ef51a
MJ Pavithra <mjpavithra6520@gmail.com>
2024-01-03 09:53:55 +0530

    Admin menu UI is completed

--- cafeteria-pos/src/App.js
+++ cafeteria-pos/src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LogInPage from "./Pages/LogInPage";
 import RegistrationPage from "./Pages/RegistrationPage";
 import AdminDashboard from "./Pages/AdminDashboard";
+import AdminMenu from './Pages/AdminMenu';
+
 
 function App() {
   return (
@@ -13,6 +15,7 @@ function App() {
           <Route path="/home" element={<h1>Home</h1>}/>
           <Route path="/register" element={<RegistrationPage/>}/>
           <Route path="/admin-dashboard" element={<AdminDashboard/>}/>
+          <Route path="/admin-menu" element={<AdminMenu/>}/>
         </Routes>
       </BrowserRouter>
   );
